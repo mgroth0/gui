7dcf81658c7e6791b74e67d3c7360bd9ef8e3904
Matt Groth <mgroth49@gmail.com>
2021-05-19 02:28:00 -0400

    pw

--- src/main/kotlin/matt/gui/loop/eventloop.kt
+++ src/main/kotlin/matt/gui/loop/eventloop.kt
@@ -1,6 +1,5 @@
 package matt.gui.loop
 
-import matt.klib.log.debug
 import java.util.concurrent.Semaphore
 
 fun <T> runLaterReturn(op: ()->T): T {
@@ -20,22 +19,12 @@ fun <T> runLaterReturn(op: ()->T): T {
 }
 
 fun runLater(s: String = "NO MESSAGE", op: ()->Unit) {
-  debug("matt.hurricanefx.runLater:${s}:scheduling")
   javafx.application.Platform.runLater {
-	debug("matt.hurricanefx.runLater:${s}:running")
 	op()
-	debug("matt.hurricanefx.runLater:${s}:finished")
   }
-  debug("matt.hurricanefx.runLater:${s}:scheduled")
 }
 
 fun <T> runLaterReturn(s: String, op: ()->T): T {
-  debug("runLaterReturn:${s}:blocking")
-  val r = runLaterReturn {
-	debug("runLaterReturn:${s}:running")
-	debug("runLaterReturn:${s}:finished")
-	op()
-  }
-  debug("runLaterReturn:${s}:returning")
+  val r = runLaterReturn { op() }
   return r
 }
