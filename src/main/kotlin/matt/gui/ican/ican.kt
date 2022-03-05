9af4674f360686100e41d88073ea71076056806d
mgroth0 <mgroth49@gmail.com>
2022-03-05 12:12:22 -0500

    autocommit

--- src/main/kotlin/matt/gui/ican/ican.kt
+++ src/main/kotlin/matt/gui/ican/ican.kt
@@ -6,7 +6,7 @@ import matt.hurricanefx.tornadofx.nodes.add
 import matt.kjlib.commons.FLOW_FOLDER
 import matt.kjlib.file.get
 
-val IconFolder = FLOW_FOLDER["icon"]
+val IconFolder by lazy { FLOW_FOLDER["icon"] }
 
 fun IconImage(file: java.io.File): Image = Image(file.toPath().toUri().toURL().toString())
 fun IconImage(file: String) = IconImage(IconFolder[file])
