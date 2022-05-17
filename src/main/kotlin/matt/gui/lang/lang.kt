3a5a95fb4e6214979dc11b3f96b8f298ea048a39
mgroth0 <mgroth49@gmail.com>
2022-05-17 15:11:58 -0400

    autocommit

--- src/main/kotlin/matt/gui/lang/lang.kt
+++ src/main/kotlin/matt/gui/lang/lang.kt
@@ -12,92 +12,103 @@ import javafx.scene.control.Control
 import javafx.scene.control.MenuItem
 import javafx.scene.input.KeyCombination
 import matt.hurricanefx.eye.lib.onChange
+import matt.hurricanefx.op
 import matt.hurricanefx.tornadofx.menu.item
 import matt.hurricanefx.tornadofx.nodes.add
 
-fun Node.setOnFocusLost(op: () -> Unit) {
-    focusedProperty().onChange {
-        if (!it) {
-            op()
-        }
-    }
-}
-fun Node.setOnFocusGained(op: () -> Unit) {
-    focusedProperty().onChange {
-        if (it) {
-            op()
-        }
-    }
+fun Node.setOnFocusLost(op: ()->Unit) {
+  focusedProperty().onChange {
+	if (!it) {
+	  op()
+	}
+  }
 }
 
-fun ActionButton(text: String, graphic: Node?=null, action: (ActionEvent) -> Unit) = Button(text, graphic).apply {
-    setOnAction {
-        action(it)
-        it.consume()
-    }
+fun Node.setOnFocusGained(op: ()->Unit) {
+  focusedProperty().onChange {
+	if (it) {
+	  op()
+	}
+  }
+}
+
+fun ActionButton(text: String, graphic: Node? = null, action: (ActionEvent)->Unit) = Button(text, graphic).apply {
+  setOnAction {
+	action(it)
+	it.consume()
+  }
 }
 
 // Buttons
-fun EventTarget.actionbutton(text: String = "", graphic: Node? = null, action: (ActionEvent) -> Unit): Button =
-    ActionButton(text, graphic, action).also{
-        add(it)
-    }
+fun EventTarget.actionbutton(text: String = "", graphic: Node? = null, action: (ActionEvent)->Unit): Button =
+  ActionButton(text, graphic, action).also {
+	add(it)
+  }
+//
+//fun EventTarget.actionbutton(text: String = "", graphic: Node? = null, action: ()->Unit): Button =
+//  actionbutton(text = text, graphic = graphic) { a: ActionEvent -> action() }
+//
+//fun EventTarget.actionbutton(text: String = "", action: ()->Unit): Button =
+//  actionbutton(text = text, graphic = null) { a: ActionEvent -> action() }
 
+
+infix fun Button.withAction(newOp: ()->Unit) = this.apply { op = newOp }
 
 fun EventTarget.removecontextmenu() {
-    if (this is Control) {
-        contextMenu = null
-    } else (this as? Node)?.apply {
-        setOnContextMenuRequested {
-        }
-    }
+  if (this is Control) {
+	contextMenu = null
+  } else (this as? Node)?.apply {
+	setOnContextMenuRequested {
+	}
+  }
 }
 
 // because when in listcells, "item" is taken
 @Suppress("unused")
 fun ContextMenu.menuitem(
-    name: String, keyCombination: KeyCombination? = null, graphic: Node? = null, op: MenuItem.() -> Unit = {}
+  name: String, keyCombination: KeyCombination? = null, graphic: Node? = null, op: MenuItem.()->Unit = {}
 ) = item(name, keyCombination, graphic, op)
 
 
 @Suppress("unused")
 fun StringProperty.appendln(s: String) {
-    append("\n" + s)
+  append("\n" + s)
 }
 
 fun StringProperty.append(s: String) {
-    set(get() + s)
+  set(get() + s)
 }
 
 @Suppress("unused")
 fun StringProperty.append(c: Char) {
-    set(get() + c)
+  set(get() + c)
 }
 
 
 fun <T> ObservableList<T>.setToSublist(start: Int, Stop: Int) {
-    setAll(subList(start, Stop).toList())
+  setAll(subList(start, Stop).toList())
 }
 
 fun <T> ObservableList<T>.removeAllButLastN(num: Int) {
-    val siz = size
-    setToSublist(siz - num, siz)
+  val siz = size
+  setToSublist(siz - num, siz)
 }
 
-fun Node.onDoubleClickConsume(action: () -> Unit) {
-    setOnMouseClicked {
-        if (it.clickCount == 2) {
-            action()
-            it.consume()
-        }
-    }
+fun Node.onDoubleClickConsume(action: ()->Unit) {
+  setOnMouseClicked {
+	if (it.clickCount == 2) {
+	  action()
+	  it.consume()
+	}
+  }
 }
+
 @Suppress("unused")
-fun Scene.onDoubleClickConsume(action: () -> Unit) {
-    setOnMouseClicked {
-        if (it.clickCount == 2) {
-            action()
-            it.consume()
-        }
-    }
+fun Scene.onDoubleClickConsume(action: ()->Unit) {
+  setOnMouseClicked {
+	if (it.clickCount == 2) {
+	  action()
+	  it.consume()
+	}
+  }
 }
