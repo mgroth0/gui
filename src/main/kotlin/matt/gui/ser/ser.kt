214940326e0d6c772fe0505598f21c89ab42fc38
mgroth0 <mgroth49@gmail.com>
2022-05-29 17:11:19 -0400

    autocommit

--- src/main/kotlin/matt/gui/ser/ser.kt
+++ src/main/kotlin/matt/gui/ser/ser.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalSerializationApi::class)
+
 package matt.gui.ser
 
 import javafx.beans.property.Property
@@ -23,16 +25,17 @@ import matt.exec.app.myDataFolder
 import matt.hurricanefx.eye.delegate.createFxProp
 import matt.hurricanefx.eye.lib.onActualChange
 import matt.kjlib.file.writeToFile
-import matt.klib.commons.get
 import matt.kjlib.map.lazyMap
 import matt.kjlib.weak.bag.WeakBag
+import matt.klib.commons.get
+import matt.klib.lang.KotlinPrimitive
 import matt.klib.lang.NEVER
 import java.io.File
 import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KProperty
 import kotlin.reflect.KProperty0
 import kotlin.reflect.KProperty1
-import kotlin.reflect.KProperty
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.isAccessible
@@ -45,11 +48,6 @@ annotation class Savable
 val savableAwareJson = Json {
   serializersModule = SerializersModule {
 	contextual(Any::class, AnySerializer)
-
-
-	//	polymorphic(Any::class) {
-	//	  subclass(SerializableWithSavables::class)
-	//	}
   }
   serializersModule
   useArrayPolymorphism
@@ -57,7 +55,7 @@ val savableAwareJson = Json {
   allowStructuredMapKeys
 }
 
-@OptIn(InternalSerializationApi::class) @Suppress("OPT_IN_OVERRIDE", "OPT_IN_USAGE") object AnySerializer:
+object AnySerializer:
   KSerializer<Any> {
   override val descriptor = object: SerialDescriptor {
 	override val elementsCount = 1
@@ -92,59 +90,27 @@ val savableAwareJson = Json {
   }
 
   override fun deserialize(decoder: Decoder): Any {
-	//	println("deserialize 1")
-	var theAny: Any? = null    //	decoder.decodeSerializableValue()
+	var theAny: Any? = null
 
 	decoder.decodeStructure(descriptor) {
-
-	  /*println("decodeSequentially=${this.decodeSequentially()}")*/    //	  this.decodeElementIndex()
-
-
-	  /*ALL I HAD TO DO WAS CALL THIS. DONT EVEN HAVE TO USE RESULT? THATS HOW YOU SKIP OVER COMMAS I THINK.*/
 	  var nextIndex = this.decodeElementIndex(descriptor)
-
-
-	  //	  println("nextIndex2=${nextIndex}")
-	  //	  println("deserialize 2")
 	  val qname = this.decodeStringElement(descriptor, 0)
-	  //	  println("deserialize 3: $qname")
-
-
-	  /*ALL I HAD TO DO WAS CALL THIS. DONT EVEN HAVE TO USE RESULT? THATS HOW YOU SKIP OVER COMMAS I THINK.*/
 	  nextIndex = this.decodeElementIndex(descriptor)
-
-
-	  //	  println("nextIndex4=${nextIndex}")
 	  theAny = this.decodeSerializableElement(descriptor, 2, findSerializerFor(qname))
-	  //	  println("deserialize 4: $theAny")
 	}
 	return theAny!!
   }
 
   override fun serialize(encoder: Encoder, value: Any) {
-	//	println("serialize1")
 	val qname = value::class.qualifiedName!!
-	//	println("qname=${qname}")
-
-	//	val holder = OutHolder(value)
-	//	val theOutAny = value as out Any
-
-
 	encoder.encodeStructure(descriptor) {
 	  encodeStringElement(descriptor, 0, qname)
-	  //	  println("wrote qname")
 	  @Suppress("UNCHECKED_CAST")
 	  encodeSerializableElement(descriptor, 1, findSerializerFor(value) as SerializationStrategy<Any>, value)
-	  //	  println("probably not getting here")
 	}
 
   }
 }
-
-class OutHolder<out T: Any>(val o: T) {
-
-}
-
 
 val saveFiles = lazyMap<KClass<*>, File> {
   myDataFolder[it::class.annotations.filterIsInstance<SaveFile>().firstOrNull()?.path
@@ -156,7 +122,7 @@ val saveFiles = lazyMap<KClass<*>, File> {
 
 )
 
-@OptIn(ExperimentalSerializationApi::class) object SerializableWithSavablesSerializer:
+object SerializableWithSavablesSerializer:
   SerializationStrategy<SerializableWithSavables<*>> {
   override val descriptor = object: SerialDescriptor {
 	override val elementsCount = 1
@@ -190,99 +156,19 @@ val saveFiles = lazyMap<KClass<*>, File> {
 	}
   }
 
-  @OptIn(InternalSerializationApi::class) override fun serialize(
+  override fun serialize(
 	encoder: Encoder, value: SerializableWithSavables<*>
-  ) {    //	value.savs::class.serializer()
-
-	//	println("SerializableWithSavablesSerializer.serialize 1")
-
+  ) {
 	encoder.encodeStructure(descriptor) {
-	  //	  println("SerializableWithSavablesSerializer.serialize 2")
-
 	  @Suppress("UNCHECKED_CAST")
 	  encodeSerializableElement(descriptor, 0, findSerializerFor(value.ser) as SerializationStrategy<Any>, value.ser!!)
-	  //	  println("SerializableWithSavablesSerializer.serialize 3")    //	  encoder.encodeStructure(descriptor) {
-	  //
-	  //	  }
-
-
-	  /*encodeSerializableElement(descriptor, 1, String.serializer(), "hey json")*/
-
 	  encodeSerializableElement(
 		descriptor, 1, MapSerializer(
 		  keySerializer = String.serializer(), valueSerializer = AnySerializer
 		), value.savs
 	  )
-
-
-	  //	  println("SerializableWithSavablesSerializer.serialize 4")    //	  encodeStringElement(descriptor, 0, qname)
-	  //	  encodeSerializableElement(descriptor, 1, theSerializer as SerializationStrategy<Any>, value)
-	}
-
-	/*encoder.encodeDouble(1.2)*/
-
-  }
-}
-
-@OptIn(ExperimentalSerializationApi::class) class MyMapSerializer<K, V>(map: Map<K, V>):
-  SerializationStrategy<Map<K, V>> {
-  override val descriptor = object: SerialDescriptor {
-	override val elementsCount = 1
-	override val kind = StructureKind.MAP
-	override val serialName = MyMapSerializer::class.qualifiedName!!
-	override fun getElementAnnotations(index: Int): List<Annotation> {
-	  require(index >= 0) { "Illegal index $index, $serialName expects only non-negative indices" }
-	  return emptyList()
-	}
-
-	override fun getElementDescriptor(index: Int): SerialDescriptor {
-	  require(index >= 0) { "Illegal index $index, $serialName expects only non-negative indices" }
-	  if (index < map.size) {
-		return this
-	  }
-	  NEVER
-
-	  //	  LinkedHashMapSerializer
-
-	}
-
-	override fun getElementIndex(name: String): Int = map.entries.indexOfFirst { it.key.toString() == name }
-
-	override fun getElementName(index: Int): String = map.entries.toList()[index].key.toString()
-
-	override fun isElementOptional(index: Int): Boolean {
-	  require(index >= 0) { "Illegal index $index, $serialName expects only non-negative indices" }
-	  return false
 	}
   }
-
-  @OptIn(InternalSerializationApi::class) override fun serialize(encoder: Encoder, value: Map<K, V>) {
-	var index = 0
-	//	println("MyMapSerializer.serialize 1")
-	encoder.encodeStructure(descriptor) {
-	  //	  println("MyMapSerializer.serialize 2")
-	  value.forEach { (_, v) ->
-		//		println("MyMapSerializer.serialize 3")
-		@Suppress("UNCHECKED_CAST")
-		encodeSerializableElement(descriptor, index, findSerializerFor(v) as SerializationStrategy<Any>, v!!)
-		//		println("MyMapSerializer.serialize 5")
-		index++
-	  }
-	}    //	encoder.encodeStructure(descriptor) {
-	//	  encodeSerializableElement(descriptor, 0, theSerializer as SerializationStrategy<Any>, value.ser)
-	//
-	//	  //	  encoder.encodeStructure(descriptor) {
-	//	  //
-	//	  //	  }
-	//
-	//
-	//	  encodeSerializableElement(
-	//		descriptor, 1, ContextualSerializer(Map::class) as SerializationStrategy<Any>, value.savs
-	//	  )
-	//	  encodeStringElement(descriptor, 0, qname)
-	//	  encodeSerializableElement(descriptor, 1, theSerializer as SerializationStrategy<Any>, value)
-  }
-
 }
 
 
@@ -294,23 +180,8 @@ inline fun <reified T: Any> T.save() {
   }
   val whole = SerializableWithSavables(this, savablePropertiesMap)
   val serializedWhole = savableAwareJson.encodeToString(SerializableWithSavablesSerializer, whole)
-  serializedWhole.
-  writeToFile(saveFiles[this::class]!!)
-} // ///*needed for reifying with autosave*/
-//internal inline fun <reified T: Any> T.save(cls: KClass<T>) {
-//  println("save1")
-//  val savablePropertiesMap = this::class.memberProperties.filter {
-//	it.hasAnnotation<Savable>()
-//  }.associate {
-//	it.name to it.call(this) as Any
-//  }
-//  println("save2")
-//  val whole = SerializableWithSavables(this, savablePropertiesMap)
-//  println("save3")
-//  val serializedWhole = savableAwareJson.encodeToString(whole)
-//  println("save4")
-//  serializedWhole.writeToFile(saveFiles[this::class]!!)
-//}
+  serializedWhole.writeToFile(saveFiles[this::class]!!)
+}
 
 
 inline fun <reified R> File.load(): R? = takeIf { it.exists() }?.readText()?.takeIf { it.isNotBlank() }?.let { json ->
@@ -320,53 +191,18 @@ inline fun <reified R> File.load(): R? = takeIf { it.exists() }?.readText()?.tak
 
   blockAutoSavingOfThese += almost.ser!!
 
-  //  println("got almost1: $almost")
   almost.savs.forEach { (k, v) ->
 	((almost.ser!!::class as KClass<*>)
 	  .memberProperties
 	  .first { it.hasAnnotation<Savable>() && it.name == k } as KMutableProperty<*>).setter.call(almost.ser, v)
-	//	  .forEach {
-	//		(it as KMutableProperty<*>).setter.call(almost.ser, v)        //		it.call()
-	//		println("got almost1.5: $almost")
-	//	  }
   }
 
-  //  println("got almost2: $almost")
   blockAutoSavingOfThese -= almost.ser!!
 
   almost.ser
 }
 
 inline fun <reified R> KClass<*>.load() = saveFiles[this]!!.load<R>()
-
-
-/*	.takeIf {
-	println("checking if exists")
-	it.exists()
-  }
-	?.readText()
-	?.takeIf {
-	  println("checking if not blank")
-	  it.isNotBlank()
-	}?.let {
-	 *//* println("load1")
-	  val r = savableAwareJson.decodeFromString<R>(it)
-	  println("load2:${r}")
-	  r
-*//*
-	  val almost = savableAwareJson.decodeFromString<SerializableWithSavables<R>>(it)
-	  almost.savs.forEach { (k, v) ->
-		(almost.ser!!::class as KClass<*>)
-		  .memberProperties
-		  .filter { it.hasAnnotation<Savable>() }
-		  .forEach {
-			it.call(almost.ser, v)
-		  }
-	  }
-	  almost.ser
-
-
-	}*/
 
 
 inline fun <reified T: Any> fx(default: T? = null, autosave: Boolean = false) =
@@ -420,13 +256,6 @@ class FXProp<V>(
 	isAccessible = true
   }.getDelegate() as FXProp<V>).fxProp
 
-//@Suppress("UNCHECKED_CAST") fun <T, V> KProperty1<T, V>.fx(t: T) = (this.apply {/*
-//  *
-//  * Caused by: kotlin.reflect.full.IllegalPropertyDelegateAccessException: Cannot obtain the delegate of a non-accessible property. Use "isAccessible = true" to make the property accessible
-//  *
-//  * */
-//  isAccessible = true
-//}.getDelegate(t) as FXProp<V>).fxProp
 
 @Suppress("UNCHECKED_CAST") fun <T, V> KProperty1<T, V>.fx(t: T) = (this.apply {/*
   *
@@ -435,25 +264,6 @@ class FXProp<V>(
   * */
   isAccessible = true
 }.getDelegate(t) as FXProp<V>).fxProp
-
-/*@Suppress("UNCHECKED_CAST") fun <R> KCallable<R>.fx(t: Any) = (this.apply {
-
-   *//*Caused by: kotlin.reflect.full.IllegalPropertyDelegateAccessException: Cannot obtain the delegate of a non-accessible property. Use "isAccessible = true" to make the property accessible*//*
-
-
-  isAccessible = true
-
-//  t::class.java.
-
-}.getDelegate(t) as FXProp<V>).fxProp*/
-
-//@Suppress("UNCHECKED_CAST") fun <T, V> kotlin.reflect.jvm.internal.KProperty1Impl<T, V>.fx(t: T) = (this.apply {/*
-//  *
-//  * Caused by: kotlin.reflect.full.IllegalPropertyDelegateAccessException: Cannot obtain the delegate of a non-accessible property. Use "isAccessible = true" to make the property accessible
-//  *
-//  * */
-//  isAccessible = true
-//}.getDelegate(t) as FXProp<V>).fxProp
 
 
 @OptIn(InternalSerializationApi::class)
@@ -468,75 +278,6 @@ fun findSerializerFor(o: Any?): KSerializer<out Any> {
 	is Double  -> Double.serializer()
 	else       -> o!!::class.serializer()
   }
-}
-
-sealed interface KotlinPrimitive {
-  val kls: KClass<*>
-}
-
-
-enum class Ints(override val kls: KClass<*>): KotlinPrimitive {
-  Byte(kotlin.Byte::class),
-  Short(kotlin.Short::class),
-  Int(kotlin.Int::class),
-  Long(kotlin.Long::class),
-}
-
-enum class IntArrays(override val kls: KClass<*>): KotlinPrimitive {
-  ByteArray(kotlin.ByteArray::class),
-  IntArray(kotlin.IntArray::class),
-  ShortArray(kotlin.ShortArray::class),
-  LongArray(kotlin.LongArray::class),
-}
-
-enum class FloatingPoints(override val kls: KClass<*>): KotlinPrimitive {
-  Float(kotlin.Float::class),
-  Double(kotlin.Double::class),
-}
-
-enum class FloatingPointArrays(override val kls: KClass<*>): KotlinPrimitive {
-  FloatArray(kotlin.FloatArray::class),
-  DoubleArray(kotlin.DoubleArray::class),
-}
-
-enum class UnsignedInts(override val kls: KClass<*>): KotlinPrimitive {
-  UByte(kotlin.UByte::class),
-  UShort(kotlin.UShort::class),
-  UInt(kotlin.UInt::class),
-  ULong(kotlin.ULong::class),
-}
-
-@OptIn(ExperimentalUnsignedTypes::class)
-enum class UnsignedIntArrays(override val kls: KClass<*>): KotlinPrimitive {
-  /*unsigned arrays and ranges*/
-  UByteArray(kotlin.UByteArray::class),
-  UShortArray(kotlin.UShortArray::class),
-  UIntArray(kotlin.UIntArray::class),
-  ULongArray(kotlin.ULongArray::class),
-}
-
-enum class Booleans(override val kls: KClass<*>): KotlinPrimitive {
-  Boolean(kotlin.Boolean::class),
-}
-
-enum class BooleanArrays(override val kls: KClass<*>): KotlinPrimitive {
-  BooleanArray(kotlin.BooleanArray::class),
-}
-
-enum class Chars(override val kls: KClass<*>): KotlinPrimitive {
-  Char(kotlin.Char::class),
-}
-
-enum class CharArrays(override val kls: KClass<*>): KotlinPrimitive {
-  CharArray(kotlin.CharArray::class),
-}
-
-enum class Strings(override val kls: KClass<*>): KotlinPrimitive {
-  String(kotlin.String::class),
-}
-
-enum class Arrays(override val kls: KClass<*>): KotlinPrimitive {
-  Array(kotlin.Array::class),
 }
 
 @OptIn(InternalSerializationApi::class)
