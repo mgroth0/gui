b240c06267ed54f76d4fe63f0842c79383b40485
mgroth0 <mgroth49@gmail.com>
2022-07-05 15:32:36 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ modtype = LIB
 dependencies {
   implementation(projects.k.json)
   api(projects.k.hurricanefx)
-  api(projects.k.fx.fxGraphics)
+  api(projects.k.fx.graphics)
   api(projects.k.exec)
   api(projects.k.auto)
 //  api(projects.k.hotkey)
