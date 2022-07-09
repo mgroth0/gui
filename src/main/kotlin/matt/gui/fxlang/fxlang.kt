835710aef65f40307328b1db4922c02601ec606f
mgroth0 <mgroth49@gmail.com>
2022-07-09 10:16:15 -0400

    autocommit

--- src/main/kotlin/matt/gui/fxlang/fxlang.kt
+++ src/main/kotlin/matt/gui/fxlang/fxlang.kt
@@ -2,14 +2,13 @@ package matt.gui.fxlang
 
 import javafx.scene.control.ListView
 import javafx.scene.control.TableView
-import javafx.scene.control.TreeTableView
-import javafx.scene.control.TreeView
 import javafx.scene.layout.Background
 import javafx.scene.layout.BackgroundFill
 import javafx.scene.paint.Paint
 import matt.hurricanefx.eye.lib.onChange
 import matt.hurricanefx.wrapper.NodeWrapper
 import matt.hurricanefx.wrapper.RegionWrapper
+import matt.hurricanefx.wrapper.TreeLikeWrapper
 import matt.klib.lang.err
 
 var RegionWrapper.backgroundFill: Paint?
@@ -34,20 +33,11 @@ fun <T> ListView<T>.onSelect(op: (T?) -> Unit) {
 }
 fun <T> NodeWrapper<ListView<T>>.onSelect(op: (T?) -> Unit) = node.onSelect(op)
 
-fun <T> TreeTableView<T>.onSelect(op: (T?) -> Unit) {
+fun <T> TreeLikeWrapper<*,T>.onSelect(op: (T?) -> Unit) {
     selectionModel.selectedItemProperty().onChange {
         op(it?.value)
     }
 }
-
-@JvmName("onSelectTreeTable") fun <T> NodeWrapper<TreeTableView<T>>.onSelect(op: (T?) -> Unit) = node.onSelect(op)
-
-fun <T> TreeView<T>.onSelect(op: (T?) -> Unit) {
-    selectionModel.selectedItemProperty().onChange {
-        op(it?.value)
-    }
-}
-@JvmName("onSelectTree") fun <T> NodeWrapper<TreeView<T>>.onSelect(op: (T?) -> Unit) = node.onSelect(op)
 
 fun <T> TableView<T>.onSelect(op: (T?) -> Unit) {
     selectionModel.selectedItemProperty().onChange {
