a73f97869aa26a10337d2f46850327715f827833
mgroth0 <mgroth49@gmail.com>
2022-10-05 16:37:00 -0400

    autocommit

--- src/main/kotlin/matt/gui/app/warmup/warmup.kt
+++ src/main/kotlin/matt/gui/app/warmup/warmup.kt
@@ -1,6 +1,6 @@
 package matt.gui.app.warmup
 
-/*the purpose of this is to have consistent performance and profiling expectations. It can be omitted but its best to keep so that I can easily distinguish actionable bottlenecks and not worry about un-actionable internal jvm warmup times*/
+/*the purpose of this is to have consistent performance and profiling expectations. It can be omitted but its best to keep so that I can easily distinguish actionable bottlenecks and not worry about un-actionable internal jvm warmup matt.math.op.times*/
 fun warmupJvmThreading() {
   Thread {
 
