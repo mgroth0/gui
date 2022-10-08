926a4887a1ba915eb296da3308b0fa65e3c0049b
mgroth0 <mgroth49@gmail.com>
2022-10-07 21:27:34 -0400

    autocommit

--- src/main/kotlin/matt/gui/exception/exception.kt
+++ src/main/kotlin/matt/gui/exception/exception.kt
@@ -9,9 +9,9 @@ import matt.exec.app.appName
 import matt.file.MFile
 import matt.fx.control.lang.actionbutton
 import matt.fx.control.mstage.ShowMode.SHOW_AND_WAIT
-import matt.auto.exception.AppUncaughtExceptionHandler.ExceptionResponse
-import matt.auto.exception.AppUncaughtExceptionHandler.ExceptionResponse.EXIT
-import matt.auto.exception.AppUncaughtExceptionHandler.ExceptionResponse.IGNORE
+import matt.log.profile.ExceptionResponse
+import matt.log.profile.ExceptionResponse.EXIT
+import matt.log.profile.ExceptionResponse.IGNORE
 import matt.fx.control.win.interact.openInNewWindow
 import matt.fx.control.wrapper.control.button.ButtonWrapper
 import matt.fx.control.wrapper.control.text.area.textarea
