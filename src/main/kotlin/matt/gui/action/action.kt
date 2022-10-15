16339509aa1df6e2ab61108c2fc5264046e390d0
mgroth0 <mgroth49@gmail.com>
2022-10-15 10:51:40 -0400

    autocommit

--- src/main/kotlin/matt/gui/action/action.kt
+++ src/main/kotlin/matt/gui/action/action.kt
@@ -1,10 +1,20 @@
 package matt.gui.action
 
+import matt.fx.graphics.wrapper.pane.grid.GridPaneWrapper
+import matt.fx.graphics.wrapper.text.text
+import matt.gui.action.button.actionButton
+import matt.lang.go
 import matt.model.idea.UserActionIdea
+import matt.model.obsmod.run.Proceeding
+import matt.obs.bind.binding
 import matt.obs.bindings.bool.ObsB
 import matt.obs.bindings.str.ObsS
 import matt.obs.prop.VarProp
 
+class ActionOnProceeding(
+  val action: GuiAction,
+  val proceeding: Proceeding?
+)
 
 interface GuiAction: UserActionIdea {
   val buttonLabel: ObsS
@@ -18,5 +28,16 @@ class GuiActionImpl(
   override val buttonLabel by lazy { VarProp(buttonLabel) }
   override fun invoke() {
 	op()
+  }
+}
+
+
+fun GridPaneWrapper<*>.actionOnProceedingRow(ap: ActionOnProceeding) {
+  row {
+	actionButton(ap.action)
+	ap.proceeding?.go { p ->
+	  text(p.status.binding { it.name })
+	  text(p.message)
+	}
   }
 }
