ecee0dcfc6ebe501991d99065aacad11ece4666f
mgroth0 <mgroth49@gmail.com>
2022-10-20 13:50:58 -0400

    autocommit

--- src/main/kotlin/matt/gui/action/action.kt
+++ src/main/kotlin/matt/gui/action/action.kt
@@ -5,13 +5,15 @@ import matt.fx.graphics.wrapper.EventTargetWrapper
 import matt.fx.graphics.wrapper.pane.grid.GridPaneWrapper
 import matt.fx.graphics.wrapper.text.text
 import matt.gui.action.button.actionButton
-import matt.lang.go
 import matt.model.idea.UserActionIdea
 import matt.model.obsmod.run.Proceeding
 import matt.obs.bind.binding
+import matt.obs.bind.deepBinding
 import matt.obs.bindings.bool.ObsB
 import matt.obs.bindings.str.ObsS
+import matt.obs.prop.ObsVal
 import matt.obs.prop.VarProp
+import matt.obs.prop.toVarProp
 
 fun EventTargetWrapper.contextMenuAction(action: GuiAction) {
   mcontextmenu {
@@ -26,8 +28,7 @@ fun EventTargetWrapper.contextMenuAction(action: GuiAction) {
 }
 
 class ActionOnProceeding(
-  val action: GuiAction,
-  val proceeding: Proceeding?
+  val action: GuiAction, val proceeding: Proceeding?
 )
 
 interface GuiAction: UserActionIdea {
@@ -46,12 +47,13 @@ class GuiActionImpl(
 }
 
 
-fun GridPaneWrapper<*>.actionOnProceedingRow(ap: ActionOnProceeding) {
+fun GridPaneWrapper<*>.actionOnProceedingRow(ap: ActionOnProceeding) = actionOnProceedingRow(ap.toVarProp())
+fun GridPaneWrapper<*>.actionOnProceedingRow(ap: ObsVal<ActionOnProceeding>) {
   row {
-	actionButton(ap.action)
-	ap.proceeding?.go { p ->
-	  text(p.status.binding { it.name })
-	  text(p.message)
-	}
+	actionButton(ap.binding { it.action })
+
+	text(ap.deepBinding { it.proceeding?.status?.binding { it.name } ?: "".toVarProp() })
+
+	text(ap.deepBinding { it.proceeding?.message ?: "".toVarProp() })
   }
 }
--- src/main/kotlin/matt/gui/action/button/button.kt
+++ src/main/kotlin/matt/gui/action/button/button.kt
@@ -1,13 +1,19 @@
 package matt.gui.action.button
 
+import matt.fx.control.wrapper.control.button.ButtonWrapper
 import matt.fx.control.wrapper.control.button.button
 import matt.fx.graphics.fxthread.ts.nonBlockingFXWatcher
 import matt.fx.graphics.wrapper.node.NW
 import matt.gui.action.GuiAction
+import matt.obs.bind.deepBinding
+import matt.obs.prop.ObsVal
+import matt.obs.prop.toVarProp
 
-fun NW.actionButton(a: GuiAction) = button(a.buttonLabel.nonBlockingFXWatcher()) {
-  enableProperty.bind(a.allowed.nonBlockingFXWatcher())
+fun NW.actionButton(a: GuiAction): ButtonWrapper = actionButton(a.toVarProp())
+
+fun NW.actionButton(a: ObsVal<GuiAction>) = button(a.deepBinding { it.buttonLabel.nonBlockingFXWatcher() }) {
+  enableProperty.bind(a.deepBinding { it.allowed.nonBlockingFXWatcher() })
   setOnAction {
-	a()
+	a.value()
   }
 }
