e78121231e8994dfab4cb88f4e0b77af5d663c11
mgroth0 <mgroth49@gmail.com>
2023-02-13 21:14:45 -0500

    autocommit

--- src/main/kotlin/matt/gui/actiontext/actiontext.kt
+++ src/main/kotlin/matt/gui/actiontext/actiontext.kt
@@ -1,10 +1,20 @@
 package matt.gui.actiontext
 
+import matt.fx.control.wrapper.label.label
 import matt.fx.graphics.wrapper.ET
 import matt.fx.graphics.wrapper.node.onLeftClick
 import matt.fx.graphics.wrapper.text.text
+import matt.fx.graphics.wrapper.text.textlike.highlightOnHover
 
 fun ET.actionText(text: String, action: ()->Unit) = text(text) {
+  highlightOnHover()
+  onLeftClick {
+	action()
+  }
+}
+
+
+fun ET.actionLabel(text: String, action: ()->Unit) = label(text) {
   highlightOnHover()
   onLeftClick {
 	action()
