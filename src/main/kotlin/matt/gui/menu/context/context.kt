84f7b1ca78829f01f2521f690c0613d56248847b
mgroth0 <mgroth49@gmail.com>
2023-04-07 19:34:39 -0400

    changed 2 files

--- src/main/kotlin/matt/gui/menu/context/context.kt
+++ src/main/kotlin/matt/gui/menu/context/context.kt
@@ -9,14 +9,11 @@ import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.control.ContextMenu
 import javafx.scene.control.MenuItem
-import javafx.scene.control.TextArea
 import javafx.scene.shape.Shape
 import matt.collect.itr.recurse.chain
 import matt.collect.map.dmap.withStoringDefault
 import matt.collect.map.lazyMap
 import matt.collect.map.sync.synchronized
-import matt.gui.menu.context.EventHandlerType.Filter
-import matt.gui.menu.context.EventHandlerType.Handler
 import matt.fx.control.wrapper.contextmenu.ContextMenuWrapper
 import matt.fx.control.wrapper.menu.MenuWrapper
 import matt.fx.control.wrapper.menu.checkitem.CheckMenuItemWrapper
@@ -30,143 +27,145 @@ import matt.fx.graphics.wrapper.node.NodeWrapper
 import matt.fx.graphics.wrapper.node.impl.NodeWrapperImpl
 import matt.fx.graphics.wrapper.node.parent.parent
 import matt.fx.graphics.wrapper.scene.SceneWrapper
-import matt.gui.interact.dialog
+import matt.gui.interact.WinOwn
 import matt.gui.interact.openInNewWindow
+import matt.gui.menu.context.EventHandlerType.Filter
+import matt.gui.menu.context.EventHandlerType.Handler
 import matt.gui.menu.context.debug.SceneDebugger
 import matt.log.tab
 import matt.log.warn.warn
 import matt.obs.prop.BindableProperty
 import matt.obs.prop.Var
 import java.lang.Thread.sleep
-import java.util.WeakHashMap
+import java.util.*
 import kotlin.concurrent.thread
 import kotlin.reflect.KClass
 
 
-fun EventTargetWrapper.mcontextmenu(op: MContextMenuBuilder.()->Unit) = MContextMenuBuilder(this.node).apply(op)
+fun EventTargetWrapper.mcontextmenu(op: MContextMenuBuilder.() -> Unit) = MContextMenuBuilder(this.node).apply(op)
 
 class MContextMenuBuilder(
-  val node: EventTarget,
-  private val isGen: Boolean = false
+    val node: EventTarget,
+    private val isGen: Boolean = false
 ) {
 
 
-  val genList = mutableListOf<MenuItemWrapper<*>>()
+    val genList = mutableListOf<MenuItemWrapper<*>>()
 
-  infix fun String.does(op: ()->Unit) = actionitem(this, op)
-  infix fun String.doesInThread(op: ()->Unit) = actionitem(this) {
-	thread {
-	  op()
-	}
-  }
+    infix fun String.does(op: () -> Unit) = actionitem(this, op)
+    infix fun String.doesInThread(op: () -> Unit) = actionitem(this) {
+        thread {
+            op()
+        }
+    }
 
-  fun actionitem(s: String, op: ()->Unit) = SimpleMenuItem(s).apply {
-	isMnemonicParsing = false
-	setOnAction {
-	  op()
-	  it.consume()
-	}
-  }.also { add(it) }
+    fun actionitem(s: String, op: () -> Unit) = SimpleMenuItem(s).apply {
+        isMnemonicParsing = false
+        setOnAction {
+            op()
+            it.consume()
+        }
+    }.also { add(it) }
 
-  fun item(s: String = "", g: NW? = null, op: MenuItemWrapper<*>.()->Unit = {}) =
-	SimpleMenuItem(s, g?.node).apply {
-	  isMnemonicParsing = false
-	  op()
-	}.also { add(it) }
-
-
-  infix fun String.toggles(b: BindableProperty<Boolean>) = checkitem(this, b)
-
-  fun checkitem(s: String, b: Var<Boolean>, op: CheckMenuItemWrapper.()->Unit = {}) =
-	CheckMenuItemWrapper(s).apply {
-	  isMnemonicParsing = false
-	  selectedProperty.bindBidirectional(b)
-	  op()
-	}.also { add(it) }
-
-  fun menu(s: String, op: MenuWrapper.()->Unit) = MenuWrapper(s).apply {
-	isMnemonicParsing = false
-	op()
-  }.also { add(it) }
-
-  fun add(item: MenuItemWrapper<*>) {
-	if (isGen) {
-	  genList.add(item)
-	} else {
-	  contextMenuItems[node]!!.add(item)
-	}
-  }
+    fun item(s: String = "", g: NW? = null, op: MenuItemWrapper<*>.() -> Unit = {}) =
+        SimpleMenuItem(s, g?.node).apply {
+            isMnemonicParsing = false
+            op()
+        }.also { add(it) }
 
 
-  fun onRequest(op: MContextMenuBuilder.()->Unit) {
-	require(!isGen)
-	contextMenuItemGens[node]!!.add(op)
+    infix fun String.toggles(b: BindableProperty<Boolean>) = checkitem(this, b)
 
-  }
-  //
-  //  fun <T: Any?> onRequest(keyGetter: ()->T, op: MContextMenuBuilder.(T)->Unit) {
-  //	require(!isGen)
-  //	contextMenuItemsByKey[node]!![key]!!.add(op(keyGetter()))
-  //  }
+    fun checkitem(s: String, b: Var<Boolean>, op: CheckMenuItemWrapper.() -> Unit = {}) =
+        CheckMenuItemWrapper(s).apply {
+            isMnemonicParsing = false
+            selectedProperty.bindBidirectional(b)
+            op()
+        }.also { add(it) }
+
+    fun menu(s: String, op: MenuWrapper.() -> Unit) = MenuWrapper(s).apply {
+        isMnemonicParsing = false
+        op()
+    }.also { add(it) }
+
+    fun add(item: MenuItemWrapper<*>) {
+        if (isGen) {
+            genList.add(item)
+        } else {
+            contextMenuItems[node]!!.add(item)
+        }
+    }
+
+
+    fun onRequest(op: MContextMenuBuilder.() -> Unit) {
+        require(!isGen)
+        contextMenuItemGens[node]!!.add(op)
+
+    }
+    //
+    //  fun <T: Any?> onRequest(keyGetter: ()->T, op: MContextMenuBuilder.(T)->Unit) {
+    //	require(!isGen)
+    //	contextMenuItemsByKey[node]!![key]!!.add(op(keyGetter()))
+    //  }
 
 
 }
 
 private fun getCMItems(node: EventTarget): List<MenuItemWrapper<*>>? {
-  val normal = contextMenuItems[node]!!
-  val gen = contextMenuItemGens[node]!!.flatMap {
-	MContextMenuBuilder(node, isGen = true).apply(it).genList
-  }
-  //  val keyGen = contextMenuItemsByKey[node]!!
-  return (normal + gen).takeIf { it.isNotEmpty() }
+    val normal = contextMenuItems[node]!!
+    val gen = contextMenuItemGens[node]!!.flatMap {
+        MContextMenuBuilder(node, isGen = true).apply(it).genList
+    }
+    //  val keyGen = contextMenuItemsByKey[node]!!
+    return (normal + gen).takeIf { it.isNotEmpty() }
 }
 
 
 abstract class RunOnce {
-  companion object {
-	var ranOnce = mutableSetOf<KClass<out RunOnce>>()
-  }
+    companion object {
+        var ranOnce = mutableSetOf<KClass<out RunOnce>>()
+    }
 
 
-  protected abstract fun run()
+    protected abstract fun run()
 
-  init {
-	if (this::class !in ranOnce) {
-	  run()
-	  ranOnce += this::class
-	}
-  }
+    init {
+        if (this::class !in ranOnce) {
+            run()
+            ranOnce += this::class
+        }
+    }
 
 }
 
 
-class CmFix: RunOnce() {
-  override fun run() {
-	/*https://bugs.openjdk.org/browse/JDK-8198497*/
-	ContextMenu.getWindows().addListener { change: Change<*> ->
-	  while (change.next()) {
-		change.addedSubList.filterIsInstance<ContextMenu>().forEach { cm ->
-		  cm.setOnShown {
-			/* I added the thread and runLater, since this still isn't working */
-			thread {
-			  sleep(100)
-			  runLater {
-				cm.sizeToScene()
-				/*IT FINALLY WORKS*/
-			  }
-			}
-		  }
-		}
-	  }
-	}
-  }
+class CmFix : RunOnce() {
+    override fun run() {
+        /*https://bugs.openjdk.org/browse/JDK-8198497*/
+        ContextMenu.getWindows().addListener { change: Change<*> ->
+            while (change.next()) {
+                change.addedSubList.filterIsInstance<ContextMenu>().forEach { cm ->
+                    cm.setOnShown {
+                        /* I added the thread and runLater, since this still isn't working */
+                        thread {
+                            sleep(100)
+                            runLater {
+                                cm.sizeToScene()
+                                /*IT FINALLY WORKS*/
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 val contextMenus = lazyMap<Scene, ContextMenuWrapper> {
-  ContextMenuWrapper().apply {
-	isAutoHide = true
-	isAutoFix = true
-  }
+    ContextMenuWrapper().apply {
+        isAutoHide = true
+        isAutoFix = true
+    }
 }
 
 
@@ -179,112 +178,116 @@ val contextMenus = lazyMap<Scene, ContextMenuWrapper> {
  *
  */
 fun SceneWrapper<*>.showMContextMenu(
-  target: Node, /*cannot be [NodeWrapper] because event targets are not wrappers?*/
-  xy: Pair<Double, Double>
+    target: Node, /*cannot be [NodeWrapper] because event targets are not wrappers?*/
+    xy: Pair<Double, Double>
 ) {
-  sequenceOf(1, 2, 3)
-  CmFix()
-  val devMenu = MenuWrapper("dev")
-  devMenu.actionitem("test exception") {
-	throw Exception("test exception")
-  }
-  devMenu.actionitem("print nodes") {
-	println("NODES:")
-	tab("TARGET:\t${target::class.simpleName}")
-	var parent: Node? = target.parent
-	while (parent != null) {
-	  tab("PARENT:\t${parent::class.simpleName}")
-	  parent = parent.parent
-	}
-  }
-  devMenu.actionitem("open FX Debugger") {
-	SceneDebugger().apply {
-	  navTo(target.wrapper as NodeWrapper)
-	}.openInNewWindow(
-	  decorated = true,
-	  alwaysOnTop = true,
-	).apply {
-	  width = 1000.0
-	  height = 1500.0
-	}
-  }
-  contextMenus[this.node].apply {
-	items.clear()
-	var node: EventTarget = target
-	while (true) {
-	  getCMItems(node)?.let {
-		if (items.isNotEmpty()) separator()
-		items += it.map { it.node }
-	  }
-	  try {
-		node = when (node) {
-		  is Parent -> node.parent ?: node.scene
-		  is Shape  -> node.parent
-		  is Canvas -> node.parent
-		  is Scene  -> node.window
-		  else      -> break
-		}
-	  } catch (e: NullPointerException) {
-		warn("got null parent in context menu generator again")
-		System.err.println("here is the stack trace:")
-		e.printStackTrace()
-		items.add(MenuItem("Got weird null parent in context menu generator! see log for stack trace"))
-		break
-	  }
-	}
-	if (items.isNotEmpty()) separator()
-	items += target.wrapped().hotkeyInfoMenu().node
-	items += devMenu.node
-  }.node.show(target, xy.first, xy.second)
+    sequenceOf(1, 2, 3)
+    CmFix()
+    val devMenu = MenuWrapper("dev")
+    devMenu.actionitem("test exception") {
+        throw Exception("test exception")
+    }
+    devMenu.actionitem("print nodes") {
+        println("NODES:")
+        tab("TARGET:\t${target::class.simpleName}")
+        var parent: Node? = target.parent
+        while (parent != null) {
+            tab("PARENT:\t${parent::class.simpleName}")
+            parent = parent.parent
+        }
+    }
+    devMenu.actionitem("open FX Debugger") {
+        SceneDebugger().apply {
+            navTo(target.wrapper as NodeWrapper)
+        }.openInNewWindow(
+            decorated = true,
+            alwaysOnTop = true,
+            title = "FX Debugger",
+            own = WinOwn.None
+        ).apply {
+            width = 1000.0
+            height = 1500.0
+        }
+    }
+    contextMenus[this.node].apply {
+        items.clear()
+        var node: EventTarget = target
+        while (true) {
+            getCMItems(node)?.let {
+                if (items.isNotEmpty()) separator()
+                items += it.map { it.node }
+            }
+            try {
+                node = when (node) {
+                    is Parent -> node.parent ?: node.scene
+                    is Shape -> node.parent
+                    is Canvas -> node.parent
+                    is Scene -> node.window
+                    else -> break
+                }
+            } catch (e: NullPointerException) {
+                warn("got null parent in context menu generator again")
+                System.err.println("here is the stack trace:")
+                e.printStackTrace()
+                items.add(MenuItem("Got weird null parent in context menu generator! see log for stack trace"))
+                break
+            }
+        }
+        if (items.isNotEmpty()) separator()
+        items += target.wrapped().hotkeyInfoMenu().node
+        items += devMenu.node
+    }.node.show(target, xy.first, xy.second)
 }
 
 enum class EventHandlerType {
-  Handler, Filter
+    Handler, Filter
 }
 
 
 private fun NodeWrapper.hotkeyInfoMenu() = MenuWrapper("Click For Hotkey Info").apply {
-  val node = this@hotkeyInfoMenu
-  fun addInfo(type: EventHandlerType) {
-	menu(
-	  when (type) {
-		Handler -> "handlers"; Filter -> "filters"
-	  }
-	) {
+    val node = this@hotkeyInfoMenu
+    fun addInfo(type: EventHandlerType) {
+        menu(
+            when (type) {
+                Handler -> "handlers"; Filter -> "filters"
+            }
+        ) {
 
 
-	  (node.chain { it.parent } + node.scene + node.stage).forEach { subNode ->
-		menu(subNode.toString()) {
-		  val h = when (type) {
-			Handler -> subNode?.hotKeyHandler
-			Filter  -> subNode?.hotKeyFilter
-		  }
-		  item("\tqp=${h?.quickPassForNormalTyping}")
-		  subNode?.hotKeyHandler?.hotkeys?.forEach { hkc ->
-			item("\t${hkc.getHotkeys().joinToString { it.toString() }}")
-		  }
-		}
-	  }
-	}
-  }
+            (node.chain { it.parent } + node.scene + node.stage).forEach { subNode ->
+                menu(subNode.toString()) {
+                    val h = when (type) {
+                        Handler -> subNode?.hotKeyHandler
+                        Filter -> subNode?.hotKeyFilter
+                    }
+                    item("\tqp=${h?.quickPassForNormalTyping}")
+                    subNode?.hotKeyHandler?.hotkeys?.forEach { hkc ->
+                        item("\t${hkc.getHotkeys().joinToString { it.toString() }}")
+                    }
+                }
+            }
+        }
+    }
 
 
-  setOnAction {
-	items.clear()
-	addInfo(Handler)
-	addInfo(Filter)
+    setOnAction {
+        items.clear()
+        addInfo(Handler)
+        addInfo(Filter)
 
 
-  }
-  //  setOnMouseClicked {
-  //
-  //  }
+    }
+    //  setOnMouseClicked {
+    //
+    //  }
 
 }
 
 
-private val contextMenuItems = WeakHashMap<EventTarget, MutableList<MenuItemWrapper<*>>>().withStoringDefault { mutableListOf() }
-  .synchronized()
+private val contextMenuItems =
+    WeakHashMap<EventTarget, MutableList<MenuItemWrapper<*>>>().withStoringDefault { mutableListOf() }
+        .synchronized()
 
-private val contextMenuItemGens = WeakHashMap<EventTarget, MutableList<MContextMenuBuilder.()->Unit>>().withStoringDefault { mutableListOf() }
-  .synchronized()
+private val contextMenuItemGens =
+    WeakHashMap<EventTarget, MutableList<MContextMenuBuilder.() -> Unit>>().withStoringDefault { mutableListOf() }
+        .synchronized()
--- src/main/kotlin/matt/gui/menu/context/debug/debug.kt
+++ src/main/kotlin/matt/gui/menu/context/debug/debug.kt
@@ -10,103 +10,138 @@ import matt.fx.graphics.wrapper.pane.grid.GridPaneWrapper
 import matt.fx.graphics.wrapper.pane.vbox.VBoxW
 import matt.fx.graphics.wrapper.region.RegionWrapper
 import matt.fx.graphics.wrapper.text.text
+import matt.fx.graphics.wrapper.text.textlike.ColoredText
 import matt.gui.actiontext.actionText
 import matt.lang.go
+import matt.obs.bind.binding
+import matt.obs.bindings.str.mybuildobs.obsString
+import matt.obs.prop.ObsVal
+import matt.prim.str.joinWithCommas
+import matt.prim.str.mybuild.string
 
 
-class SceneDebugger: VBoxW() {
+class SceneDebugger : VBoxW() {
 
-  var lastNav: NodeDebugger? = null
+    var lastNav: NodeDebugger? = null
 
-  fun navTo(nw: NodeWrapper) = navTo(NodeDebugger(this, nw))
-  fun navTo(nd: NodeDebugger) {
-	(lastNav?.debugNode as? RegionWrapper<*>)?.border = null
-	lastNav = nd
-	(nd.debugNode as? RegionWrapper<*>)?.yellow()
-	clear()
-	val s = nd.debugNode.scene
-	text("scene: $s")
-	val r = s?.root
-	if (r == null) {
-	  text("root: $r")
-	} else {
-	  actionText("root: $r") {
-		navTo(r)
-	  }
-	}
-	+nd
-  }
+    fun navTo(nw: NodeWrapper) = navTo(NodeDebugger(this, nw))
+    fun navTo(nd: NodeDebugger) {
+        (lastNav?.debugNode as? RegionWrapper<*>)?.border = null
+        lastNav = nd
+        (nd.debugNode as? RegionWrapper<*>)?.yellow()
+        clear()
+        val s = nd.debugNode.scene
+        text("scene: $s")
+        val r = s?.root
+        if (r == null) {
+            text("root: $r")
+        } else {
+            actionText("root: $r") {
+                navTo(r)
+            }
+        }
+        +nd
+    }
 }
 
 class NodeDebugger(
-  debugger: SceneDebugger,
-  val debugNode: NodeWrapper
-): VBoxW() {
-  init {
+    debugger: SceneDebugger,
+    val debugNode: NodeWrapper
+) : VBoxW() {
+    init {
 
-	val p = debugNode.parent
-	actionText("parent: $p") {
-	  if (p != null) {
-		debugger.navTo(p)
-	  }
-	}
-	val sibs = (p as? RegionWrapper<*>)?.children
-	val previousSib = sibs?.getOrNull(sibs.indexOf(debugNode) - 1)
-	val nextSib = sibs?.getOrNull(sibs.indexOf(debugNode) + 1)
-	actionText("previous sibling: $previousSib") {
-	  previousSib?.go {
-		debugger.navTo(previousSib)
-	  }
-	}
-	actionText("next sibling: $nextSib") {
-	  nextSib?.go {
-		debugger.navTo(nextSib)
-	  }
-	}
-	actionText("node: $debugNode") {
-	  debugger.navTo(debugNode)
-	}
-	if (debugNode is RegionWrapper<*>) {
-	  val n = debugNode
-	  text("grid row: ${GridPane.getRowIndex(debugNode.node)}")
-	  text("grid col: ${GridPane.getColumnIndex(debugNode.node)}")
-	  text("height: ${debugNode.height}")
-	  text("min height: ${debugNode.minHeight}")
-	  text("pref height: ${debugNode.prefHeight}")
-	  text("max height: ${debugNode.maxHeight}")
+        val p = debugNode.parent
+        actionText("parent: $p") {
+            if (p != null) {
+                debugger.navTo(p)
+            }
+        }
+        val sibs = (p as? RegionWrapper<*>)?.children
+        val previousSib = sibs?.getOrNull(sibs.indexOf(debugNode) - 1)
+        val nextSib = sibs?.getOrNull(sibs.indexOf(debugNode) + 1)
+        actionText("previous sibling: $previousSib") {
+            previousSib?.go {
+                debugger.navTo(previousSib)
+            }
+        }
+        actionText("next sibling: $nextSib") {
+            nextSib?.go {
+                debugger.navTo(nextSib)
+            }
+        }
+        actionText("node: $debugNode") {
+            debugger.navTo(debugNode)
+        }
+        fun staticProp(
+            label: String,
+            value: Any?
+        ) = text(string {
+            +"$label: "
+            +value
+        })
 
-	  text("width: ${debugNode.width}")
-	  text("min width: ${debugNode.minWidth}")
-	  text("pref width: ${debugNode.prefWidth}")
-	  text("max width: ${debugNode.maxWidth}")
+        fun dynamicProp(
+            label: String,
+            value: ObsVal<*>
+        ) = text(obsString {
+            appendStatic("$label: ")
+            +value
+        }) {
+            fill = Color.LIGHTBLUE
+        }
+        dynamicProp("visible", debugNode.visibleProperty)
+        dynamicProp("managed", debugNode.managedProperty)
+        dynamicProp("style", debugNode.styleProperty)
+        dynamicProp("style class", debugNode.styleClass.binding { it.joinWithCommas() })
+        dynamicProp("layout x", debugNode.layoutXProperty)
+        dynamicProp("layout y", debugNode.layoutYProperty)
+        staticProp("grid row", GridPane.getRowIndex(debugNode.node))
+        staticProp("grid col", GridPane.getColumnIndex(debugNode.node))
+        if (debugNode is ColoredText) {
+            dynamicProp("text", debugNode.textProperty)
+            dynamicProp("text fill", debugNode.textFillProperty)
+        }
+        if (debugNode is RegionWrapper<*>) {
+            val n = debugNode
 
-	  text("layoutX: ${debugNode.layoutX}")
-	  text("layoutY: ${debugNode.layoutY}")
+            dynamicProp("height", debugNode.heightProperty)
+            dynamicProp("min height", debugNode.minHeightProperty)
+            dynamicProp("pref height", debugNode.prefHeightProperty)
+            dynamicProp("max height", debugNode.maxHeightProperty)
 
-	  text("visible: ${debugNode.isVisible}")
-	  text("managed: ${debugNode.isManaged}")
-	  checkbox("blue") {
-		selectedProperty.onChange {
-		  if (it) n.blue() else {
-			n.border = null
-		  }
-		}
-	  }
-	  actionbutton("green background") {
-		n.backgroundFill = Color.GREEN
-	  }
-	  if (debugNode is GridPaneWrapper<*>) {
-		checkbox("gridLines", debugNode.gridLinesVisibleProp)
-	  }
-	  debugNode.children.takeIf { it.isNotEmpty() }?.go { it ->
-		text("children:")
-		it.forEach {
-		  actionText("\t${it}") {
-			debugger.navTo(it)
-		  }
-		}
-	  }
-	}
+            dynamicProp("width", debugNode.widthProperty)
 
-  }
+            dynamicProp("min width", debugNode.minWidthProperty)
+            dynamicProp("pref width", debugNode.prefWidthProperty)
+            dynamicProp("max width", debugNode.maxWidthProperty)
+
+
+
+
+
+            checkbox("blue") {
+                selectedProperty.onChange {
+                    if (it) n.blue() else {
+                        n.border = null
+                    }
+                }
+            }
+            actionbutton("green background") {
+                n.backgroundFill = Color.GREEN
+            }
+            if (debugNode is GridPaneWrapper<*>) {
+                checkbox("gridLines", debugNode.gridLinesVisibleProp)
+            }
+            debugNode.children.takeIf { it.isNotEmpty() }?.go { it ->
+                text("children:")
+                it.forEach {
+                    actionText("\t${it}") {
+                        debugger.navTo(it)
+                    }
+                }
+            }
+        }
+
+
+    }
 }
